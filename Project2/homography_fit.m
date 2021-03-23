cd0f02c5621493bf0cc99fb169da958bf9f53cf5
jhess <4jhess@gmail.com>
2021-03-23 14:57:26 -0400

    Add files via upload

--- Project2/homography_fit.m
+++ Project2/homography_fit.m
@@ -1,10 +1,11 @@
 function N = homography_fit(P, H, tau)
-%% pass points through homography
-p1 = [P(:,1:2), ones(size(P,1), 1)].';
-p2hat = (H * p1).';
+% pass points through homography
+p1 = [P(:,1:2), ones(size(P,1), 1)];
+%p2hat = (H * p1).';
+p2hat = apply_homography(p1, H);
 p2hat = p2hat(:,1:2);
 
-%% compare to found correspondences
+% compare to found correspondences
 p2 = P(:,3:4);
 err = sum((p2 - p2hat).^2, 2);
 N = err < tau;
