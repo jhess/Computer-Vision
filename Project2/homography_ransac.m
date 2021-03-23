68fa9d77c603e5839bea3df99519962efaa1c3cf
jhess <4jhess@gmail.com>
2021-03-23 15:01:05 -0400

    Add files via upload

--- Project2/homography_ransac.m
+++ Project2/homography_ransac.m
@@ -1,25 +1,25 @@
 function H = homography_ransac(P, tau, max_iter, alpha)
-%% begin the RANSAC iterations
+% begin the RANSAC iterations
 best_match = 0;
 good_enough = @(N) N > alpha * size(P,1);
 for n = 1:max_iter
-    %% pick points
+    % pick points
     idx = randperm(size(P,1), 4);
     Phat = P(idx, :);
-    %% find a homography
+    % find a homography
     Hhat = homography(Phat);
     
-    %% check the number of matching points
+    % check the number of matching points
     fit = homography_fit(P, Hhat, tau);
     Nhat = sum(fit);
     
-    %% check for termination conditions
+    % check for termination conditions
     if good_enough(Nhat)
         H = homography(P(fit, :));
         return
     end
     
-    %% update best match
+    % update best match
     if Nhat > best_match
         best_match = Nhat;
         best_idx = fit;
